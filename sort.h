0c76c5482819faf2a549f6652d9471eb174687b4
Wave08 <mararakudakwashe@gmail.com>
2022-08-18 15:22:08 -0700

    task0

--- sort.h
+++ sort.h
@@ -16,5 +16,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void swap(int *arr, int idx1, int idx2);
 void print_array(const int *aray, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+listint_t *create_listint(const int *array, size_t size);
+void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 #endif
